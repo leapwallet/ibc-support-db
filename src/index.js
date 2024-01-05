c0f3bb4a1520e41fbf7457a657f59643d2110928
baryon2 <opthami@gmail.com>
2024-01-05 13:06:15 +0530

    Fix the script to use ping pub api to get chain registry data

--- src/index.js
+++ src/index.js
@@ -7,10 +7,11 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
+const PINGPUB_BASEURL = 'https://registry.ping.pub/_IBC/'
+const ATOMSCAN_BASEURL = 'https://proxy.atomscan.com/directory/_IBC/'
 
-const main = async () => {
-  try {
-    const response = await fetch('https://proxy.atomscan.com/directory/_IBC/');
+async function fetchFromAtomScan(){
+    const response = await fetch(ATOMSCAN_BASEURL);
     const rawHTML = await response.text();
     const root = parse(rawHTML);
     const elements = root.querySelectorAll('.line > a[href$=".json"]');
@@ -33,6 +34,46 @@ const main = async () => {
         }
       }
     });
+
+  return {
+    baseUrl: ATOMSCAN_BASEURL,
+    hrefs,
+    ibcSupport
+  }
+}
+
+async function fetchFromPingPub(){
+    const response = await fetch(PINGPUB_BASEURL);
+    const ibcData = await response.json();
+    const hrefs = ibcData.filter(file => file.name).map((file) => {
+      return file.name
+    })
+    const ibcSupport = {}
+    hrefs.forEach((href) => {
+      const [src, dest] = href.slice(0, -5).split('-');
+       if (ibcSupport[src]) {
+          ibcSupport[src][dest] = true;
+        } else {
+          ibcSupport[src] = { [dest]: true };
+        }
+        if (ibcSupport[dest]) {
+          ibcSupport[dest][src] = true;
+        } else {
+          ibcSupport[dest] = { [src]: true };
+        }
+    })
+
+  return {
+    baseUrl: PINGPUB_BASEURL,
+    hrefs,
+    ibcSupport
+  }
+}
+
+
+const main = async () => {
+  try {
+    const { baseUrl, ibcSupport, hrefs } = await fetchFromPingPub() 
     const allData = Object.entries(ibcSupport).reduce((acc, [key, value]) => {
       return {
         ...acc,
@@ -52,7 +93,7 @@ const main = async () => {
     await Promise.all(
       hrefs.map(async (href) => {
         const res = await fetch(
-          `https://proxy.atomscan.com/directory/_IBC/${href}`
+          `${baseUrl}/${href}`
         );
         const data = await res.json();
         return fs.writeFile(
