270283395bdd8db8a083490dd7721b7ccf75d56e
Himanshu Singh <himanshusingh@Himanshus-MacBook-Air.local>
2024-02-06 21:39:06 +0530

    revert linting

--- src/index.js
+++ src/index.js
@@ -7,71 +7,70 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
-const PINGPUB_BASEURL = 'https://registry.ping.pub/_IBC/';
-const ATOMSCAN_BASEURL = 'https://proxy.atomscan.com/directory/_IBC/';
+const PINGPUB_BASEURL = 'https://registry.ping.pub/_IBC/'
+const ATOMSCAN_BASEURL = 'https://proxy.atomscan.com/directory/_IBC/'
 const COSMOS_CHAIN_REGISTRY_BASE_URL =
   'https://api.github.com/repos/cosmos/chain-registry/git/trees/c67a1fa68ddbd7393111bf78a4f9faefdc515a3d';
 
-async function fetchFromAtomScan() {
-  const response = await fetch(ATOMSCAN_BASEURL);
-  const rawHTML = await response.text();
-  const root = parse(rawHTML);
-  const elements = root.querySelectorAll('.line > a[href$=".json"]');
-  const ibcSupport = {};
-  const hrefs = [];
-  elements.forEach((element) => {
-    const href = element.getAttribute('href');
-    if (href) {
-      hrefs.push(href);
-      const [src, dest] = href.slice(0, -5).split('-');
-      if (ibcSupport[src]) {
-        ibcSupport[src][dest] = true;
-      } else {
-        ibcSupport[src] = { [dest]: true };
+
+async function fetchFromAtomScan(){
+    const response = await fetch(ATOMSCAN_BASEURL);
+    const rawHTML = await response.text();
+    const root = parse(rawHTML);
+    const elements = root.querySelectorAll('.line > a[href$=".json"]');
+    const ibcSupport = {};
+    const hrefs = [];
+    elements.forEach((element) => {
+      const href = element.getAttribute('href');
+      if (href) {
+        hrefs.push(href);
+        const [src, dest] = href.slice(0, -5).split('-');
+        if (ibcSupport[src]) {
+          ibcSupport[src][dest] = true;
+        } else {
+          ibcSupport[src] = { [dest]: true };
+        }
+        if (ibcSupport[dest]) {
+          ibcSupport[dest][src] = true;
+        } else {
+          ibcSupport[dest] = { [src]: true };
+        }
       }
-      if (ibcSupport[dest]) {
-        ibcSupport[dest][src] = true;
-      } else {
-        ibcSupport[dest] = { [src]: true };
-      }
-    }
-  });
+    });
 
   return {
     baseUrl: ATOMSCAN_BASEURL,
     hrefs,
-    ibcSupport,
-  };
+    ibcSupport
+  }
 }
 
-async function fetchFromPingPub() {
-  const response = await fetch(PINGPUB_BASEURL);
-  const ibcData = await response.json();
-  const hrefs = ibcData
-    .filter((file) => file.name)
-    .map((file) => {
-      return file.name;
-    });
-  const ibcSupport = {};
-  hrefs.forEach((href) => {
-    const [src, dest] = href.slice(0, -5).split('-');
-    if (ibcSupport[src]) {
-      ibcSupport[src][dest] = true;
-    } else {
-      ibcSupport[src] = { [dest]: true };
-    }
-    if (ibcSupport[dest]) {
-      ibcSupport[dest][src] = true;
-    } else {
-      ibcSupport[dest] = { [src]: true };
-    }
-  });
+async function fetchFromPingPub(){
+    const response = await fetch(PINGPUB_BASEURL);
+    const ibcData = await response.json();
+    const hrefs = ibcData.filter(file => file.name).map((file) => {
+      return file.name
+    })
+    const ibcSupport = {}
+    hrefs.forEach((href) => {
+      const [src, dest] = href.slice(0, -5).split('-');
+       if (ibcSupport[src]) {
+          ibcSupport[src][dest] = true;
+        } else {
+          ibcSupport[src] = { [dest]: true };
+        }
+        if (ibcSupport[dest]) {
+          ibcSupport[dest][src] = true;
+        } else {
+          ibcSupport[dest] = { [src]: true };
+        }
+    })
 
   return {
     baseUrl: PINGPUB_BASEURL,
     hrefs,
-    ibcSupport,
-  };
+    ibcSupport
+  }
 }
 
 async function fetchFromCosmosChainRegistry() {
@@ -107,7 +106,7 @@ async function fetchFromCosmosChainRegistry() {
 
 const main = async () => {
   try {
-    const { baseUrl, ibcSupport, hrefs } = await fetchFromCosmosChainRegistry();
+    const { baseUrl, ibcSupport, hrefs } = await fetchFromCosmosChainRegistry() 
     const allData = Object.entries(ibcSupport).reduce((acc, [key, value]) => {
       return {
         ...acc,
@@ -126,7 +125,9 @@ const main = async () => {
     );
     await Promise.all(
       hrefs.map(async (href) => {
-        const res = await fetch(`${baseUrl}/${href}`);
+        const res = await fetch(
+          `${baseUrl}/${href}`
+        );
         const data = await res.json();
         return fs.writeFile(
           path.join(__dirname, '../data/pairs', href),
